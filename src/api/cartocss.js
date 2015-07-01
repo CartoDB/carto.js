75eeb8c0332d7472412ec514913324f9724c2f85
Francisco Dans <fdansv@gmail.com>
2015-07-01 15:43:35 +0200

    adds torque css generation

--- src/api/cartocss.js
+++ src/api/cartocss.js
@@ -61,6 +61,35 @@ var CSS = {
       }
     }
     return css;
+  },
+
+  torque: function(stats, tableName){
+    var tableID = "#" + tableName;
+    var css = [
+        '/** category visualization */',
+        'Map {',
+        '  -torque-time-attribute: ' + stats.column + ';',
+        '  -torque-aggregation-function: "count(cartodb_id)";',
+        '  -torque-frame-count: 128;',
+        '  -torque-animation-duration: 15;',
+        '  -torque-resolution: 2',
+        '}',
+        tableID,
+        '  marker-width: 3;',
+        '  marker-fill-opacity: 0.8;',
+        '  marker-fill: #FEE391; ',
+        '  comp-op: "lighten";',
+        '  [value > 2] { marker-fill: #FEC44F; }',
+        '  [value > 3] { marker-fill: #FE9929; }',
+        '  [value > 4] { marker-fill: #EC7014; }',
+        '  [value > 5] { marker-fill: #CC4C02; }',
+        '  [value > 6] { marker-fill: #993404; }',
+        '  [value > 7] { marker-fill: #662506; }',
+        '  [frame-offset = 1] { marker-width: 10; marker-fill-opacity: 0.05;}',
+        '  [frame-offset = 2] { marker-width: 15; marker-fill-opacity: 0.02;}',
+        '}'
+    ].join('\n');
+    return css;
   }
 }
 
