7282c9b56a2327e068a87a16e84f57fbb761beaf
Pablo Alonso Garcia <alonsogarciapablo@gmail.com>
2015-07-23 18:04:33 +0200

    Show warning if cartodb.js is not used within a browser

--- src/node.js
+++ src/node.js
@@ -1,3 +1,6 @@
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = cartodb;
 }
+if (typeof window === 'undefined') {
+  console.warning('cartodb.js is not supported as a node module but intended to be used in a browser environment');
+}
