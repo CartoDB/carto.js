de68d7f8978411b8888c46f51a0335ec52b50322
Ivan Malagon <hacheka@gmail.com>
2018-06-15 15:24:12 +0200

    Remove wax cache

--- vendor/wax.cartodb.js
+++ vendor/wax.cartodb.js
@@ -3329,58 +3329,23 @@ wax.g.connector = function(options) {
 // Get a tile element from a coordinate, zoom level, and an ownerDocument.
 wax.g.connector.prototype.getTile = function(coord, zoom, ownerDocument) {
     var key = zoom + '/' + coord.x + '/' + coord.y;
-    var img = null;
     if (!this.cache[key]) {
-      this.cache[key] = new Image(256, 256);
-      img = this.cache[key];
+      var img = this.cache[key] = new Image(256, 256);
       this.cache[key].src = this.getTileUrl(coord, zoom);
       this.cache[key].setAttribute('gTileKey', key);
       this.cache[key].setAttribute("style","opacity: "+this.opacity+"; filter: alpha(opacity="+(this.opacity*100)+");");
       this.cache[key].onerror = function() { img.style.display = 'none'; };
-      this.cache[key].setAttribute('calls', '1');
-    } else {
-      try {
-        var calls = parseInt(this.cache[key].getAttribute('calls'), 10);
-        calls++;
-        this.cache[key].setAttribute('calls', calls.toString());
-      } catch (exc) {
-        // Swallow
-      }
-      
     }
-    img = this.cache[key];
-
-    return img;
+    return this.cache[key];
 };
 
 // Remove a tile that has fallen out of the map's viewport.
 //
 // TODO: expire cache data in the gridmanager.
 wax.g.connector.prototype.releaseTile = function(tile) {
-    var key = tile.getAttribute('gTileKey');
-    var removeTile = false;
-
-    if (this.cache[key]) {
-      var currentCalls = 2; // To avoid removing an element in case we can't get the calls. Ultradefensive.
-      try {
-        currentCalls = parseInt(this.cache[key].getAttribute('calls'), 10);
-      } catch (exc) {
-        // Swallow
-      }
-      currentCalls--;
-      if (currentCalls <= 0) {
-        delete this.cache[key];
-        removeTile = true;
-      } else {
-        this.cache[key].setAttribute('calls', currentCalls.toString());
-      }
-    } else {
-      removeTile = true;
-    }
-
-    if (removeTile && tile.parentNode) {
-      tile.parentNode.removeChild(tile);
-    }
+  // var key = tile.getAttribute('gTileKey');
+  // if (this.cache[key]) delete this.cache[key];
+  // if (tile.parentNode) tile.parentNode.removeChild(tile);
 };
 
 // Get a tile url, based on x, y coordinates and a z value.
