0843288cca8571f0b00a37ebfe442360c312b011
elenatorro <elenatorro@gmail.com>
2019-01-10 14:39:15 +0100

    Fix tomtom geocoder

--- src/geo/geocoders/tomtom_geocoder.js
+++ src/geo/geocoders/tomtom_geocoder.js
@@ -53,50 +53,50 @@ cdb.geo.geocoder.TOMTOM = {
     var bestCandidate = rawResponse.results[0];
 
     return [{
-      boundingbox: _getBoundingBox(bestCandidate),
-      center: _getCenter(bestCandidate),
-      type: _getType(bestCandidate)
+      boundingbox: this._getBoundingBox(bestCandidate),
+      center: this._getCenter(bestCandidate),
+      type: this._getType(bestCandidate)
     }];
+  },
+
+  /**
+   * TomTom returns { lon, lat } while we use [lat, lon]
+   */
+  _getCenter: function (result) {
+    return [result.position.lat, result.position.lon];
+  },
+
+  /**
+   * Transform the feature type into a well known enum.
+   */
+  _getType: function (result) {
+    var type = result.type;
+
+    if (this.TYPES[type]) {
+      if (type === 'Geography' && result.entityType) {
+        type = type + ':' + result.entityType;
+      }
+      return this.TYPES[type];
+    }
+
+    return 'default';
+  },
+
+  /**
+   * Transform the feature bbox into a carto.js well known format.
+   */
+  _getBoundingBox: function (result) {
+    if (!result.viewport) {
+      return;
+    }
+    var upperLeft = result.viewport.topLeftPoint;
+    var bottomRight = result.viewport.btmRightPoint;
+
+    return {
+      south: bottomRight.lat,
+      west: upperLeft.lon,
+      north: upperLeft.lat,
+      east: bottomRight.lon
+    };
   }
 };
-
-/**
- * TomTom returns { lon, lat } while we use [lat, lon]
- */
-function _getCenter(result) {
-  return [result.position.lat, result.position.lon];
-}
-
-/**
- * Transform the feature type into a well known enum.
- */
-function _getType(result) {
-  var type = result.type;
-
-  if (this.TYPES[type]) {
-    if (type === 'Geography' && result.entityType) {
-      type = type + ':' + result.entityType;
-    }
-    return this.TYPES[type];
-  }
-
-  return 'default';
-}
-
-/**
- * Transform the feature bbox into a carto.js well known format.
- */
-function _getBoundingBox(result) {
-  if (!result.viewport) {
-    return;
-  }
-  var upperLeft = result.viewport.topLeftPoint;
-  var bottomRight = result.viewport.btmRightPoint;
-
-  return {
-    south: bottomRight.lat,
-    west: upperLeft.lon,
-    north: upperLeft.lat,
-    east: bottomRight.lon
-  };
-}
