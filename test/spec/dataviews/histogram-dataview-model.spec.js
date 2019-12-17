cbb27180e174dca0e9f41a3455def999324e41d9
Víctor Velarde <victor.velarde@gmail.com>
2019-12-17 20:34:21 +0100

    Add polygon filter to histogram dataview

--- test/spec/dataviews/histogram-dataview-model.spec.js
+++ test/spec/dataviews/histogram-dataview-model.spec.js
@@ -2,6 +2,7 @@ var Backbone = require('backbone');
 var WindshaftFiltersRange = require('../../../src/windshaft/filters/range');
 var WindshaftFiltersBoundingBox = require('../../../src/windshaft/filters/bounding-box');
 var WindshaftFiltersCircle = require('../../../src/windshaft/filters/circle');
+var WindshaftFiltersPolygon = require('../../../src/windshaft/filters/polygon');
 var HistogramDataviewModel = require('../../../src/dataviews/histogram-dataview-model');
 var MapModelBoundingBoxAdapter = require('../../../src/geo/adapters/map-model-bounding-box-adapter');
 var helper = require('../../../src/dataviews/helpers/histogram-helper');
@@ -519,6 +520,50 @@ describe('dataviews/histogram-dataview-model', function () {
       var updatedCircleEncoded = encodeURIComponent(JSON.stringify(updatedCircle));
       expect(this.model.set('url', 'http://example.com'));
       expect(this.model.url()).toEqual('http://example.com?circle=' + updatedCircleEncoded + '&' + apiKeyQueryParam);
+    });
+
+    it('should include polygon filter, plus other required params', function () {
+      var filter = new WindshaftFiltersPolygon();
+      var polygon = { type: 'Polygon', coordinates: [[1, 2], [3, 4], [5, 6], [1, 2]] };
+      filter.setPolygon(polygon);
+
+      this.model = new HistogramDataviewModel({
+        source: this.source
+      }, {
+        engine: engineMock,
+        polygonFilter: filter
+      });
+
+      // DataviewModel defaults set this prop to true, even for cases like this not requiring passing a bbox filter
+      this.model.set('sync_on_bbox_change', false);
+
+      var polygonEncoded = encodeURIComponent(JSON.stringify(polygon));
+      expect(this.model.set('url', 'http://example.com'));
+      expect(this.model.url()).toEqual('http://example.com?polygon=' + polygonEncoded + '&' + apiKeyQueryParam);
+    });
+
+    it('should update polygon filter', function () {
+      var filter = new WindshaftFiltersPolygon();
+      var polygon = { type: 'Polygon', coordinates: [[1, 2], [3, 4], [5, 6], [1, 2]] };
+      filter.setPolygon(polygon);
+
+      this.model = new HistogramDataviewModel({
+        source: this.source
+      }, {
+        engine: engineMock,
+        polygonFilter: filter
+      });
+      // DataviewModel defaults set this prop to true, even for cases like this not requiring passing a bbox filter
+      this.model.set('sync_on_bbox_change', false);
+
+      // updated!
+      var updatedPolygon = { type: 'Polygon', coordinates: [[10, 20], [30, 40], [50, 60], [10, 20]] };
+
+      filter.setPolygon(updatedPolygon);
+
+      var updatedPolygonEncoded = encodeURIComponent(JSON.stringify(updatedPolygon));
+      expect(this.model.set('url', 'http://example.com'));
+      expect(this.model.url()).toEqual('http://example.com?polygon=' + updatedPolygonEncoded + '&' + apiKeyQueryParam);
     });
 
     describe('column type is number', function () {
